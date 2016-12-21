51f1c3be3211358848d0a85c296ffadf3432a1f7
LYU KUN YU <davidlu0629@users.noreply.github.com>
2016-12-21 09:19:59 +0800

    Update en.py

--- en.py
+++ en.py
@@ -1,5 +1,7 @@
+#you can use this code to split your data in your like
 file = open ('hw3.tr')
 for  line in file:
 	if line[0] == '+' :
+		#strip>>discard string '\n'   split>>split data in line by blank
 		temp = line.strip('\n').split(' ')
 		print  temp
